9b69b8e1e266fa77c9f007796487dc41ce82295e
Sergio Calderón <bs.sergiocalderon@gmail.com>
2024-04-14 20:16:17 -0300

    Deleting custom lib path (run_all_sizes)

--- opt-7/run_all_sizes.sh
+++ opt-7/run_all_sizes.sh
@@ -3,10 +3,8 @@
 # Arg 2: number of repetitions
 # Arg 3: number of threads
 
-# Set env variables and clean old files
+# Set env variables
 PROGRAM_NAME=$1
-LD_LIBRARY_PATH=/home/mcostanzo/lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 
  # Run tests
 for size in 100 500 1500
@@ -15,6 +13,6 @@ do
 
 	for ((i=1; i<=$2; i++))
 	do
-		RUSTFLAGS='-L /home/mcostanzo/lib' cargo run --example $PROGRAM_NAME --no-default-features --release -q $3 "../gem-${size}mb.csv"
+		cargo run --example $PROGRAM_NAME --no-default-features --release -q $3 "../gem-${size}mb.csv"
 	done
 done
