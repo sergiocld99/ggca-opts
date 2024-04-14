2e5dd4258167b5a0f4eaac5f93714813fa52619b
Sergio Calderón <bs.sergiocalderon@gmail.com>
2024-04-14 20:15:01 -0300

    Deleting custom lib path (run_tests)

--- opt-1/run_tests.sh
+++ opt-1/run_tests.sh
@@ -4,10 +4,8 @@
 # Arg 3: dataset size in MB (100, 500 or 1500)
 # Arg 4: number of threads
 
-# Set env variables and clean old files
+# Set env variables
 PROGRAM_NAME=$1
-LD_LIBRARY_PATH=/home/mcostanzo/lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 
  # Run tests
 for T in $4
@@ -16,6 +14,6 @@ do
 
 	for ((i=1; i<=$2; i++))
 	do
-		RUSTFLAGS='-L /home/mcostanzo/lib' cargo run --example $PROGRAM_NAME --no-default-features --release -q $T "../gem-$3mb.csv"
+		cargo run --example $PROGRAM_NAME --no-default-features --release -q $T "../gem-$3mb.csv"
 	done
 done
