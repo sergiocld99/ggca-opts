b3a05d49193f7fe67ec4e36c605ef95bcd2e1420
Sergio Calderón <bs.sergiocalderon@gmail.com>
2024-04-23 05:17:42 -0300

    Scripts de comparación de Opts
    Se agregó un script bash para cada uno de los métodos de correlación, en
    el cual para un determinado tamaño de dataset y cantidad de hilos se
    repite la ejecución entre Opt-1 y la máxima optimización (Opt-4, 6 o 7,
    segun corresponda).

---
+++ run_kendalls.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Arg 1: number of repetitions
+# Arg 2: dataset size in MB (100, 500 or 1500)
+# Arg 3: number of threads
+
+# Set env variables
+PROGRAM_NAME=single-kendalls
+
+ # Run tests
+for VERSION in opt-1 opt-2 opt-3 opt-4 opt-5 opt-6
+do
+	echo "$VERSION, $PROGRAM_NAME: Testing T=$3"
+	cd $VERSION
+	cargo build --example $PROGRAM_NAME --no-default-features --release -q
+
+	for ((i=1; i<=$1; i++))
+	do
+		./target/release/examples/$PROGRAM_NAME $3 "../gem-$2mb.csv"
+		#cargo run --example $PROGRAM_NAME --no-default-features --release -q $4 "../gem-$3mb.csv"
+	done
+
+	cd ../
+done
